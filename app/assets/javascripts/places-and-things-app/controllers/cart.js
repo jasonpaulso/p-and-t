e8700e9d16946ae259fb9eb597163ba1f8983491
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-08-10 11:44:51 -0700

    reviewing cart_response data

--- app/assets/javascripts/places-and-things-app/controllers/cart.js
+++ app/assets/javascripts/places-and-things-app/controllers/cart.js
@@ -3,6 +3,8 @@ PlacesAndThings
     function CartCtrl($scope, $rootScope, $http) {
       $http.get('api/store/current_cart')
         .then(function(response) {
-        console.log(response);
+        
+        $scope.current_cart = response.data;
+        console.log($scope.current_cart);
       });
     }
