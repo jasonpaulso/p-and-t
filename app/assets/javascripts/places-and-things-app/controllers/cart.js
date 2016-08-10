72c70eb299ceb0f173f3b6301a3e77b91fc06785
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-08-10 13:36:12 -0700

    added rootscope cart quantity variable

--- app/assets/javascripts/places-and-things-app/controllers/cart.js
+++ app/assets/javascripts/places-and-things-app/controllers/cart.js
@@ -2,21 +2,20 @@ PlacesAndThings
     .controller('CartCtrl', ['$scope', '$rootScope', '$http', 'CartService',
     function($scope, $rootScope, $http, CartService) {
 
-
+      $rootScope.totalItems = ""
       this.current_cart = CartService.retrieveCart()
       .then(function(response) {
           $scope.current_cart = response.data;
-          // cart_item_details($scope.current_cart[0]);
+          $rootScope.totalItems = $scope.totalItems();
+          $scope.totalItems = $scope.totalItems();
         })
 
-        var cart_item_details = function(item) {
-          $http.get('/api/products/' + item.id)
-            .then(function(response) {
-            $scope.cart_item_detail = response.data;
-            console.log($scope.cart_item_detail);
-          });
-        }
-        // cart_item_details(this.current_cart[0]);
-
+        $scope.totalItems = function() {
+          var totalItems = 0;
+          for(var i=0; i<$scope.current_cart.length; i++){
+            totalItems = totalItems + $scope.current_cart[i].quantity
+          }
+          return totalItems;
+        };
 
     }]);
