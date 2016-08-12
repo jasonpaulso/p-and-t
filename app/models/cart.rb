d3f09bab5a5be4c62cc427e76d54745794624100
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-08-12 11:25:46 -0700

    removed to_json method from cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,24 +2,6 @@ class Cart < ApplicationRecord
   has_many :cart_products
   has_many :products, through: :cart_products
 
-  def cart_json
-    cart_array = []
-    products = [] 
-      cart_products.each do |product|
-        cart_array << {
-              id: product.product.id,
-              name: product.product.name, 
-              price: product.product.price,
-              description: product.product.description,
-              quantity: product.quantity,
-              item_total: product.total
-            }
-      end
-      # cart_array << {total_items: total_items}
-      return cart_array
-  end
-
-
   def total_items 
     total = 0
     self.cart_products.each do |product|
