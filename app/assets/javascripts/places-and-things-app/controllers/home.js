e8bd66270b8d2aad452d242b94e368d82767ea1a
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-08-18 10:20:02 -0700

    random quote generator built

--- app/assets/javascripts/places-and-things-app/controllers/home.js
+++ app/assets/javascripts/places-and-things-app/controllers/home.js
@@ -4,23 +4,27 @@ PlacesAndThings
   
   function($scope, $rootScope, CartService, $http) {
 
-
-
-
-    $scope.quotesArray = [
-      {quote: '“To awaken alone in a strange town is one of the pleasantest sensations in the world."',
-      attribution: '—Freya Stark'
+    var quotesArray = [
+      {quote: 'To awaken alone in a strange town is one of the pleasantest sensations in the world.',
+      attribution: 'Freya Stark'
       },
-      {quote: '“For my part, I travel not to go anywhere, but to go. I travel for travel’s sake. The great affair is to move.”',
-      attribution: '—Robert Louis Stevenson'},
+      {quote: 'For my part, I travel not to go anywhere, but to go. I travel for travel’s sake. The great affair is to move.',
+      attribution: 'Robert Louis Stevenson'},
+      {quote: 'We travel, some of us forever, to seek other places, other lives, other souls.',
+      attribution: 'Anais Nin'},
+      {quote: 'A journey is best measured in friends, rather than miles.',
+        attribution: 'Tim Cahill' },
+              {quote: 'Travel makes one modest. You see what a tiny place you occupy in the world.',
+        attribution: 'Gustave Flaubert' },
+              {quote: 'The life you have led doesn’t need to be the only life you have.',
+        attribution: 'Anna Quindlen' },
+              {quote: 'Broad, wholesome, charitable views of men and things cannot be acquired by vegetating in one little corner of the earth all of one’s lifetime.',
+        attribution: 'Mark Twain'}
       ];
 
-    $scope.quote = {
+    var rand = Math.floor((Math.random() * quotesArray.length) + 0);
 
-      quote: $scope.quotesArray[1].quote,
-      attribution: $scope.quotesArray[1].attribution
-
-    };
+    $scope.quote = quotesArray[rand];
 
     $scope.checkForUser = function() {
       CartService.retrieveCart()
