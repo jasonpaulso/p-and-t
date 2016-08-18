9a51fb71bed8704fa052a3f890d089eee183f9da
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-08-18 10:35:01 -0700

    styling email contact form:

--- app/assets/javascripts/places-and-things-app/controllers/home.js
+++ app/assets/javascripts/places-and-things-app/controllers/home.js
@@ -1,41 +1,42 @@
 PlacesAndThings
 
 .controller('HomeCtrl', ['$scope', '$rootScope', 'CartService', '$http',
-  
+
   function($scope, $rootScope, CartService, $http) {
+
+    var home = $scope;
 
     var quotesArray = [
       {quote: 'To awaken alone in a strange town is one of the pleasantest sensations in the world.',
-      attribution: 'Freya Stark'
-      },
+      attribution: 'Freya Stark'},
       {quote: 'For my part, I travel not to go anywhere, but to go. I travel for travel’s sake. The great affair is to move.',
       attribution: 'Robert Louis Stevenson'},
       {quote: 'We travel, some of us forever, to seek other places, other lives, other souls.',
       attribution: 'Anais Nin'},
       {quote: 'A journey is best measured in friends, rather than miles.',
-        attribution: 'Tim Cahill' },
-              {quote: 'Travel makes one modest. You see what a tiny place you occupy in the world.',
-        attribution: 'Gustave Flaubert' },
-              {quote: 'The life you have led doesn’t need to be the only life you have.',
-        attribution: 'Anna Quindlen' },
-              {quote: 'Broad, wholesome, charitable views of men and things cannot be acquired by vegetating in one little corner of the earth all of one’s lifetime.',
-        attribution: 'Mark Twain'}
-      ];
+      attribution: 'Tim Cahill' },
+      {quote: 'Travel makes one modest. You see what a tiny place you occupy in the world.',
+      attribution: 'Gustave Flaubert' },
+      {quote: 'The life you have led doesn’t need to be the only life you have.',
+      attribution: 'Anna Quindlen' },
+      {quote: 'Broad, wholesome, charitable views of men and things cannot be acquired by vegetating in one little corner of the earth all of one’s lifetime.',
+      attribution: 'Mark Twain'}
+    ];
 
     var rand = Math.floor((Math.random() * quotesArray.length) + 0);
 
-    $scope.quote = quotesArray[rand];
+    home.quote = quotesArray[rand];
 
-    $scope.checkForUser = function() {
+    home.checkForUser = function() {
       CartService.retrieveCart()
       .then(function(response){
-        $scope.userEmail = response.data.user_email;
+        home.userEmail = response.data.user_email;
 
       })
 
     }
 
-      $scope.submitForm = function(user) {
+    home.submitForm = function(user) {
       CartService.updateCartUser(user)
       .then(function(response){
         $rootScope.$broadcast("UpdateCart");
@@ -43,7 +44,7 @@ PlacesAndThings
 
     }
 
-    $scope.checkForUser();
+    home.checkForUser();
 
   }]);
 
