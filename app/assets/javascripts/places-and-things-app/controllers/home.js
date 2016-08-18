1090e1044db2a3a9a0634825db8825294da7ba83
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-08-18 10:06:16 -0700

    building random quote generator in home controller

--- app/assets/javascripts/places-and-things-app/controllers/home.js
+++ app/assets/javascripts/places-and-things-app/controllers/home.js
@@ -4,18 +4,32 @@ PlacesAndThings
   
   function($scope, $rootScope, CartService, $http) {
 
-    // $rootScope.userEmail = "";
+
+
+
+    $scope.quotesArray = [
+      {quote: '“To awaken alone in a strange town is one of the pleasantest sensations in the world."',
+      attribution: '—Freya Stark'
+      },
+      {quote: '“For my part, I travel not to go anywhere, but to go. I travel for travel’s sake. The great affair is to move.”',
+      attribution: '—Robert Louis Stevenson'},
+      ];
+
+    $scope.quote = {
+
+      quote: $scope.quotesArray[1].quote,
+      attribution: $scope.quotesArray[1].attribution
+
+    };
 
     $scope.checkForUser = function() {
       CartService.retrieveCart()
       .then(function(response){
         $scope.userEmail = response.data.user_email;
-        // console.log(response.data.user_email);
+
       })
 
     }
-
-      
 
       $scope.submitForm = function(user) {
       CartService.updateCartUser(user)
