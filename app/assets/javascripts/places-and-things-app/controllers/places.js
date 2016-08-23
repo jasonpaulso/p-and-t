ca492e8a53d4f89bacd4ae04bad9078c346543d4
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-08-22 20:29:09 -0700

    removed commented code from places.js

--- app/assets/javascripts/places-and-things-app/controllers/places.js
+++ app/assets/javascripts/places-and-things-app/controllers/places.js
@@ -4,25 +4,6 @@ PlacesAndThings
 
   function($scope, $stateParams, PlaceService, $http) {
 
-    var place = $scope;
-    // var placeId = $stateParams.placeId;
-
-  // PlaceService.placeShow(1)
-  //   .then(function(response) {
-  //   place.destinations = response.data.destinations;
-  // });
-
-// place.getPlace();
-
-var url = 'api/place/';
-// var placeId = $stateParams.placeId;
-
-  // $http.get(url + placeId)
-  //   .then(function(response) {
-  //   place.destinations = response.data.destinations;
-  //   console.log(response);
-  // });
-  // console.log(placeId);
 
 }]);
 
