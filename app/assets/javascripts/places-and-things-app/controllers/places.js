6e60aebc0a79956649580edc8633f3c46c41d07d
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-08-23 18:32:45 -0700

    fixed places links in navbar. ng-repeat for cities is too slow and requires refresh to show

--- app/assets/javascripts/places-and-things-app/controllers/places.js
+++ app/assets/javascripts/places-and-things-app/controllers/places.js
@@ -6,14 +6,13 @@ PlacesCtrl.$inject = ['$scope','PlaceService'];
 function PlacesCtrl($scope, PlaceService) {
 
   places = $scope
-
+  places.cities = [];
 
   places.getPlaces = function() 
   {PlaceService.placesIndex()
     .then(function(response) {
-      places.cities = response.data
+      places.cities = response.data;
     });}
-
 
     places.getPlaces();
   };
