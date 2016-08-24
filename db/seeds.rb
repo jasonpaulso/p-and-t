354d18b17eeda520606cea6375a96c2e5bd5021e
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-08-23 17:42:53 -0700

    added links to seed file and added 'target=_blank' to place show page links

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,15 +23,15 @@
     blurb: "Copenhagen is the capital and most populated city of Denmark. It has a municipal population of 591,481 and a larger urban population of 1,280,371 (as of 1 January 2016). The Copenhagen metropolitan area has just over 2 million inhabitants. The city is situated on the eastern coast of the island of Zealand; another small portion of the city is located on Amager, and is separated from Malmö, Sweden, by the strait of Øresund.")
   
   copenhagen_destination_list = 
-  [{name:"Almanak",category:eat,image:"Almanak.jpg"},
-    {name:"Copenhagen Street Food",category:eat,image:"CopenhagenStreetFood.jpeg"},
-    {name:"KÖD",category:eat,image:"KoD.jpg"},
-    {name:"Hay House",category:shop,image:"HayHouse.jpg"},
-    {name:"Playtype",category:shop,image:"Playtype.jpg"},
-    {name:"Nyhavn",category:see,image:"Nyhavn.jpeg"},
-    {name:"The Round Tower",category:see,image:"TheRoundTower.jpeg"},
-    {name:"Tivoli",category:see,image:"Tivoli.jpeg"},
-    {name:"Tortus",category:shop,image:"Tortus.jpg"}]
+  [{name:"Almanak",category:eat,image:"Almanak.jpg", link:"http://thestandardcph.dk/en/almanak"},
+    {name:"Copenhagen Street Food",category:eat,image:"CopenhagenStreetFood.jpeg", link:"http://copenhagenstreetfood.dk/en/"},
+    {name:"KÖD",category:eat,image:"KoD.jpg", link:"http://koedkbh.com/"},
+    {name:"Hay House",category:shop,image:"HayHouse.jpg", link:"http://hay.dk/#/site/furniture/new"},
+    {name:"Playtype",category:shop,image:"Playtype.jpg", link:"https://playtype.com/"},
+    {name:"Nyhavn",category:see,image:"Nyhavn.jpeg", link:"http://www.visitcopenhagen.com/copenhagen/nyhavn-gdk474735"},
+    {name:"The Round Tower",category:see,image:"TheRoundTower.jpeg", link:"http://www.visitcopenhagen.com/copenhagen/round-tower-gdk410741"},
+    {name:"Tivoli",category:see,image:"Tivoli.jpeg", link:"https://www.tivoli.dk/en"},
+    {name:"Tortus",category:shop,image:"Tortus.jpg", link:"http://tortus-copenhagen.com/"}]
 
   copenhagen_destination_list.each do |destination|
     copenhagen.destinations.create(destination)
@@ -47,15 +47,15 @@
     blurb: "Stockholm is the capital of Sweden and the most populous city in the Nordic countries, with 925 934 people living in the municipality, approximately 1.4 million in the urban area, and 2.2 million in the metropolitan area. The city is spread across 14 islands on the coast in the southeast of Sweden at the mouth of Lake Mälaren, by the Stockholm archipelago and the Baltic Sea. The area has been settled since the Stone Age, in the 6th millennium BC, and was founded as a city in 1252 by a Swedish statesman Birger Jarl. It is also the capital of Stockholm County.")
   
   stockholm_destination_list = 
-  [{name:"Knut",category:eat,image:"Eat_Knut.jpg"},
-    {name:"Malarpaviljongen",category:eat,image:"Eat_Malarpaviljongen.jpg"},
-    {name:"Rosendals Tradgard",category:eat,image:"Eat_RosendalsTradgard.jpg"},
-    {name:"Archipelago",category:see,image:"See_Archipelago.jpeg"},
-    {name:"City Hall",category:see,image:"See_CityHall.jpg"},
-    {name:"Skansen",category:see,image:"See_Skansen.jpg"},
-    {name:"Byredo",category:shop,image:"Shop_Byredo.jpg"},
-    {name:"Konst-ig",category:shop,image:"Shop_Konst-ig.jpg"},
-    {name:"Nitty Gritty",category:shop,image:"Shop_NittyGritty.jpg"}]
+  [{name:"Knut",category:eat,image:"Eat_Knut.jpg", link:"http://restaurangknut.se/sv"},
+    {name:"Malarpaviljongen",category:eat,image:"Eat_Malarpaviljongen.jpg", link:"http://malarpaviljongen.se/"},
+    {name:"Rosendals Tradgard",category:eat,image:"Eat_RosendalsTradgard.jpg", link:"http://www.rosendalstradgard.se/"},
+    {name:"Stockholm Archipelago",category:see,image:"See_Archipelago.jpeg", link:"http://www.stockholmarchipelago.se/en/"},
+    {name:"City Hall",category:see,image:"See_CityHall.jpg", link:"http://international.stockholm.se/the-city-hall/"},
+    {name:"Skansen",category:see,image:"See_Skansen.jpg", link:"http://www.skansen.se/en/kategori/english"},
+    {name:"Byredo",category:shop,image:"Shop_Byredo.jpg", link:"https://byredo.com/"},
+    {name:"Konst-ig",category:shop,image:"Shop_Konst-ig.jpg", link:"http://www.konstig.se/"},
+    {name:"Nitty Gritty",category:shop,image:"Shop_NittyGritty.jpg", link: "https://www.nittygrittystore.com/"}]
 
   stockholm_destination_list.each do |destination|
     stockholm.destinations.create(destination)
@@ -71,15 +71,15 @@
     blurb: "Helsinki is the capital and largest city of Finland. It is in the region of Uusimaa, in southern Finland, on the shore of the Gulf of Finland. Helsinki has a population of 629,512, an urban population of 1,214,210, and a metropolitan population of over 1.4 million, making it the most populous municipality and urban area in Finland. Helsinki is located some 80 kilometres (50 mi) north of Tallinn, Estonia, 400 km (250 mi) east of Stockholm, Sweden, and 388 km (241 mi) west of Saint Petersburg, Russia. Helsinki has close historical connections with these three cities.")
   
   helsinki_destination_list = 
-  [{name:"B Smokery",category:eat,image:"Dine_BSmokery.jpg"},
-    {name:"Tori",category:eat,image:"Dine_Tori.JPG"},
-    {name:"Vanha Kauppahalli",category:eat,image:"Dine_VanhaKauppahalli.jpg"},
-    {name:"Kiasma",category:see,image:"See_Kiasma.jpeg"},
-    {name:"Sibelius Monument",category:see,image:"See_SibeliusMonument.JPG"},
-    {name:"The Senate Square",category:see,image:"See_TheSenateSquare.jpg"},
-    {name:"Iitalla",category:shop,image:"Shop_Iittala.jpg"},
-    {name:"Kauniste",category:shop,image:"Shop_Kauniste.jpg"},
-    {name:"Marimekko",category:shop,image:"Shop_Marimekko.jpg"}]
+  [{name:"B Smokery",category:eat,image:"Dine_BSmokery.jpg", link:"http://bsmokery.fi/"},
+    {name:"Tori",category:eat,image:"Dine_Tori.JPG", link:"http://www.ravintolatori.fi/punavuori/"},
+    {name:"Vanha Kauppahalli",category:eat,image:"Dine_VanhaKauppahalli.jpg", link:"http://vanhakauppahalli.fi/en/"},
+    {name:"Kiasma",category:see,image:"See_Kiasma.jpeg", link:"http://www.kiasma.fi/en/"},
+    {name:"Sibelius Monument",category:see,image:"See_SibeliusMonument.JPG", link:"http://www.visithelsinki.fi/en/whats-on/for-cruise-visitors/sibelius-monument"},
+    {name:"The Senate Square",category:see,image:"See_TheSenateSquare.jpg", link:"http://www.visithelsinki.fi/en/see-and-experience/sights-and-attractions/senate-square"},
+    {name:"Iitalla",category:shop,image:"Shop_Iittala.jpg", link:"https://www.iittala.com/home"},
+    {name:"Kauniste",category:shop,image:"Shop_Kauniste.jpg", link:"http://www.kauniste.com/aboutus/"},
+    {name:"Marimekko",category:shop,image:"Shop_Marimekko.jpg", link:"https://us.marimekko.com/#US"}]
 
   helsinki_destination_list.each do |destination|
     helsinki.destinations.create(destination)
